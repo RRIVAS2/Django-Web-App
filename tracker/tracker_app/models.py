6ac72fac8e4c8e3b475f56f40f141df73a9522cd
RRIVAS2 <ricardorl1990@gmail.com>
2026-07-18 14:26:16 -0600

    Created index.html structure, and activity and activitylog models

--- tracker/tracker_app/models.py
+++ tracker/tracker_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Activity(models.Model):
+    """
+    Model representing an activity that wastes time (e.g., "Doom Scrolling", "Watching TV")."""
+    name = models.CharField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class ActivityLog(models.Model):
+    """
+    Model represent a log entry from a user for a specific activity. Each log entry is associated with an activity and contains the duration of time spent on that activity.
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+
+    duration_minutes = models.PositiveIntegerField()
+
+    logged_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.activity.name} - {self.duration_minutes} minutes - {self.logged_at} "
--- tracker/tracker_app/views.py
+++ tracker/tracker_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Activity, ActivityLog
+from django.db.models import Sum
+
 
 # Create your views here.
+
+
+def index(request):
+    """
+    View function for the home page of the tracker app.
+    """
+
+    user_logs = ActivityLog.objects.filter(user=request.user).order_by('-logged_at') if request.user.is_authenticated else []
+
+    all_activities = Activity.objects.all()
+
+    duration_minutes_total_calculation = user_logs.aggregate(calculation=Sum('duration_minutes'), default=0)
+
+    total_duration = duration_minutes_total_calculation['calculation']
+
+    context = {
+        'logs': user_logs,
+        'activities': all_activities,
+        'total_duration': total_duration
+    }
+
+    return render(request, 'tracker_app/index.html', context)
